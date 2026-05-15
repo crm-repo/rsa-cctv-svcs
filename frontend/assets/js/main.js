4d63187e96b3086ac8c0f8d7f79f62d47f4a939e
John Bernas <jhannbernas@yahoo.com>
2026-05-16 01:13:07 +0800

    Updated Hamburger to open as overlay/drawer from right

--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -1,9 +1,12 @@
 const mobileMenuButton = document.getElementById("mobileMenuButton");
 const mobileMenu = document.getElementById("mobileMenu");
+const mobileMenuOverlay = document.getElementById("mobileMenuOverlay");
 
 mobileMenuButton.addEventListener("click", () => {
   mobileMenu.classList.toggle("hidden");
   mobileMenu.classList.toggle("flex");
+
+  mobileMenuOverlay.classList.toggle("hidden");
 
   if (mobileMenu.classList.contains("hidden")) {
     mobileMenuButton.innerHTML = "☰";
@@ -12,12 +15,24 @@ mobileMenuButton.addEventListener("click", () => {
   }
 });
 
+mobileMenuOverlay.addEventListener("click", () => {
+  mobileMenu.classList.add("hidden");
+  mobileMenu.classList.remove("flex");
+
+  mobileMenuOverlay.classList.add("hidden");
+
+  mobileMenuButton.innerHTML = "☰";
+});
+
 const mobileMenuLinks = mobileMenu.querySelectorAll("a");
 
 mobileMenuLinks.forEach((link) => {
   link.addEventListener("click", () => {
     mobileMenu.classList.add("hidden");
     mobileMenu.classList.remove("flex");
+
+    mobileMenuOverlay.classList.add("hidden");
+
     mobileMenuButton.innerHTML = "☰";
   });
 });
